d0eca78bebeb6ff46eead5299fae2327b83b9c4a
Jiachi Liu <inbox@huozhi.im>
2025-08-07 17:14:40 +0200

    turbopack handling

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -2464,7 +2464,7 @@ async function renderToStream(
       getErrorRSCPayload,
       tree,
       ctx,
-      reactServerErrorsByDigest.has((err as any)?.digest) ? null : err,
+      reactServerErrorsByDigest.has((err as any).digest) ? null : err,
       errorType
     )
 
@@ -2990,7 +2990,7 @@ async function spawnDynamicValidationInDev(
                 finalServerReactController.signal.aborted &&
                 isPrerenderInterruptedError(err)
               ) {
-                return err?.digest
+                return err.digest
               }
 
               if (isReactLargeShellError(err)) {
@@ -4432,7 +4432,7 @@ async function prerenderToStream(
       getErrorRSCPayload,
       tree,
       ctx,
-      reactServerErrorsByDigest.has((err as any)?.digest) ? undefined : err,
+      reactServerErrorsByDigest.has((err as any).digest) ? undefined : err,
       errorType
     )
 
