d7b6e28ee26f089d5c98f5ec8cce77ddd73031e1
Hendrik Liebau <mail@hendrik-liebau.de>
2025-08-18 14:48:10 +0200

    Use debugChannel in browser (for Turbopack)

--- packages/next/src/client/dev/hot-reloader/app/hot-reloader-app.tsx
+++ packages/next/src/client/dev/hot-reloader/app/hot-reloader-app.tsx
@@ -446,6 +446,12 @@ function processMessage(
       dispatcher.onDevToolsConfig(obj.data)
       return
     }
+    case HMR_ACTIONS_SENT_TO_BROWSER.REACT_DEBUG_CHUNK: {
+      window.__NEXT_REACT_DEBUG_CHUNKS_CONTROLLER.enqueue(
+        Buffer.from(obj.base64EncodedChunk, 'base64')
+      )
+      return
+    }
     default: {
       obj satisfies never
     }
@@ -454,16 +460,18 @@ function processMessage(
 
 export default function HotReload({
   assetPrefix,
+  requestId,
   children,
   globalError,
 }: {
   assetPrefix: string
+  requestId: string
   children: ReactNode
   globalError: GlobalErrorState
 }) {
   useErrorHandler(dispatcher.onUnhandledError, dispatcher.onUnhandledRejection)
 
-  const webSocketRef = useWebsocket(assetPrefix)
+  const webSocketRef = useWebsocket(assetPrefix, requestId)
 
   useWebsocketPing(webSocketRef)
   const sendMessage = useSendMessage(webSocketRef)
