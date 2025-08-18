413f89385e7c98e465ef55bbf7eb6ee94f6e7a44
Hendrik Liebau <mail@hendrik-liebau.de>
2025-08-18 18:34:28 +0200

    Define `debugChannel` for RSC requests

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -239,6 +239,7 @@ export type AppRenderContext = {
   appUsingSizeAdjustment: boolean
   flightRouterState?: FlightRouterState
   requestId: string
+  htmlRequestId: string
   pagePath: string
   clientReferenceManifest: DeepReadonly<ClientReferenceManifest>
   assetPrefix: string
@@ -285,6 +286,7 @@ interface ParsedRequestHeaders {
   readonly isRSCRequest: boolean
   readonly nonce: string | undefined
   readonly previouslyRevalidatedTags: string[]
+  readonly htmlRequestId: string | undefined
 }
 
 function parseRequestHeaders(
@@ -329,6 +331,11 @@ function parseRequestHeaders(
     options.previewModeId
   )
 
+  const htmlRequestId =
+    typeof headers[NEXT_REQUEST_ID_HEADER] === 'string'
+      ? headers[NEXT_REQUEST_ID_HEADER]
+      : undefined
+
   return {
     flightRouterState,
     isPrefetchRequest,
@@ -339,6 +346,7 @@ function parseRequestHeaders(
     isDevWarmupRequest,
     nonce,
     previouslyRevalidatedTags,
+    htmlRequestId,
   }
 }
 
@@ -551,17 +559,30 @@ async function generateDynamicFlightRenderResult(
     temporaryReferences?: WeakMap<any, string>
   }
 ): Promise<RenderResult> {
-  const renderOpts = ctx.renderOpts
+  const {
+    clientReferenceManifest,
+    componentMod,
+    htmlRequestId,
+    renderOpts,
+    requestId,
+    workStore,
+  } = ctx
+
+  const {
+    dev = false,
+    onInstrumentationRequestError,
+    sendReactDebugChunk,
+  } = renderOpts
 
   function onFlightDataRenderError(err: DigestedError) {
-    return renderOpts.onInstrumentationRequestError?.(
+    return onInstrumentationRequestError?.(
       err,
       req,
       createErrorContext(ctx, 'react-server-components-payload')
     )
   }
   const onError = createFlightReactServerErrorHandler(
-    !!renderOpts.dev,
+    dev,
     onFlightDataRenderError
   )
 
@@ -579,18 +600,25 @@ async function generateDynamicFlightRenderResult(
   // which contains the subset React.
   const flightReadableStream = workUnitAsyncStorage.run(
     requestStore,
-    ctx.componentMod.renderToReadableStream,
+    componentMod.renderToReadableStream,
     RSCPayload,
-    ctx.clientReferenceManifest.clientModules,
+    clientReferenceManifest.clientModules,
     {
       onError,
       temporaryReferences: options?.temporaryReferences,
       filterStackFrame,
+      debugChannel: {
+        writable: new WritableStream<Uint8Array>({
+          write(chunk) {
+            sendReactDebugChunk?.(chunk, htmlRequestId, requestId)
+          },
+        }),
+      },
     }
   )
 
   return new FlightRenderResult(flightReadableStream, {
-    fetchMetrics: ctx.workStore.fetchMetrics,
+    fetchMetrics: workStore.fetchMetrics,
   })
 }
 
@@ -1332,14 +1360,12 @@ function App<T>({
   clientReferenceManifest,
   ServerInsertedHTMLProvider,
   nonce,
-  requestId,
 }: {
   reactServerStream: BinaryStreamOf<T>
   preinitScripts: () => void
   clientReferenceManifest: NonNullable<RenderOpts['clientReferenceManifest']>
   ServerInsertedHTMLProvider: React.ComponentType<{ children: JSX.Element }>
   nonce?: string
-  requestId: string
 }): JSX.Element {
   preinitScripts()
   const response = React.use(
@@ -1382,7 +1408,6 @@ function App<T>({
           actionQueue={actionQueue}
           globalErrorState={response.G}
           assetPrefix={response.p}
-          requestId={requestId}
         />
       </ServerInsertedHTMLProvider>
     </HeadManagerContext.Provider>
@@ -1398,14 +1423,12 @@ function ErrorApp<T>({
   clientReferenceManifest,
   ServerInsertedHTMLProvider,
   nonce,
-  requestId,
 }: {
   reactServerStream: BinaryStreamOf<T>
   preinitScripts: () => void
   clientReferenceManifest: NonNullable<RenderOpts['clientReferenceManifest']>
   ServerInsertedHTMLProvider: React.ComponentType<{ children: JSX.Element }>
   nonce?: string
-  requestId: string
 }): JSX.Element {
   preinitScripts()
   const response = React.use(
@@ -1439,7 +1462,6 @@ function ErrorApp<T>({
         actionQueue={actionQueue}
         globalErrorState={response.G}
         assetPrefix={response.p}
-        requestId={requestId}
       />
     </ServerInsertedHTMLProvider>
   )
@@ -1624,16 +1646,6 @@ async function renderToHTMLOrFlightImpl(
   query = { ...query }
   stripInternalQueries(query)
 
-  const {
-    flightRouterState,
-    isPrefetchRequest,
-    isRuntimePrefetchRequest,
-    isRSCRequest,
-    isDevWarmupRequest,
-    isHmrRefresh,
-    nonce,
-  } = parsedRequestHeaders
-
   const { isStaticGeneration } = workStore
 
   let requestId: string
@@ -1651,6 +1663,17 @@ async function renderToHTMLOrFlightImpl(
   }
 
   res.setHeader(NEXT_REQUEST_ID_HEADER, requestId)
+
+  const {
+    flightRouterState,
+    isPrefetchRequest,
+    isRuntimePrefetchRequest,
+    isRSCRequest,
+    isDevWarmupRequest,
+    isHmrRefresh,
+    nonce,
+    htmlRequestId = requestId,
+  } = parsedRequestHeaders
 
   /**
    * Dynamic parameters. E.g. when you visit `/dashboard/vercel` which is rendered by `/dashboard/[slug]` the value will be {"slug": "vercel"}.
@@ -1685,6 +1708,7 @@ async function renderToHTMLOrFlightImpl(
     appUsingSizeAdjustment,
     flightRouterState,
     requestId,
+    htmlRequestId,
     pagePath,
     clientReferenceManifest,
     assetPrefix,
@@ -2086,7 +2110,8 @@ async function renderToStream(
   postponedState: PostponedState | null,
   metadata: AppPageRenderResultMetadata
 ): Promise<ReadableStream<Uint8Array>> {
-  const { assetPrefix, nonce, pagePath, renderOpts, requestId } = ctx
+  const { assetPrefix, htmlRequestId, nonce, pagePath, renderOpts, requestId } =
+    ctx
 
   const {
     basePath,
@@ -2100,6 +2125,7 @@ async function renderToStream(
     onInstrumentationRequestError,
     page,
     reactMaxHeadersLength,
+    sendReactDebugChunk,
     shouldWaitOnAllReady,
     subresourceIntegrityManifest,
     supportsDynamicResponse,
@@ -2238,7 +2264,7 @@ async function renderToStream(
                 debugChannel: {
                   writable: new WritableStream<Uint8Array>({
                     write(chunk) {
-                      renderOpts.sendReactDebugChunk?.(ctx.requestId, chunk)
+                      sendReactDebugChunk?.(chunk, htmlRequestId, requestId)
                     },
                   }),
                 },
@@ -2318,6 +2344,13 @@ async function renderToStream(
           {
             filterStackFrame,
             onError: serverComponentsErrorHandler,
+            debugChannel: {
+              writable: new WritableStream<Uint8Array>({
+                write(chunk) {
+                  sendReactDebugChunk?.(chunk, htmlRequestId, requestId)
+                },
+              }),
+            },
           }
         )
       )
@@ -2363,7 +2396,6 @@ async function renderToStream(
             clientReferenceManifest={clientReferenceManifest}
             ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
             nonce={nonce}
-            requestId={requestId}
           />,
           postponed,
           { onError: htmlRendererErrorHandler, nonce }
@@ -2403,7 +2435,6 @@ async function renderToStream(
         clientReferenceManifest={clientReferenceManifest}
         ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
         nonce={nonce}
-        requestId={requestId}
       />,
       {
         onError: htmlRendererErrorHandler,
@@ -2563,7 +2594,6 @@ async function renderToStream(
               preinitScripts={errorPreinitScripts}
               clientReferenceManifest={clientReferenceManifest}
               nonce={nonce}
-              requestId={requestId}
             />
           ),
           streamOptions: {
@@ -2659,7 +2689,6 @@ async function _spawnDynamicValidationInDev(
     implicitTags,
     nonce,
     renderOpts,
-    requestId,
     workStore,
   } = ctx
 
@@ -2909,7 +2938,6 @@ async function _spawnDynamicValidationInDev(
         clientReferenceManifest={clientReferenceManifest}
         ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
         nonce={nonce}
-        requestId={requestId}
       />,
       {
         signal: initialClientReactController.signal,
@@ -3138,7 +3166,6 @@ async function _spawnDynamicValidationInDev(
               clientReferenceManifest={clientReferenceManifest}
               ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
               nonce={nonce}
-              requestId={requestId}
             />,
             {
               signal: finalClientReactController.signal,
@@ -3245,7 +3272,6 @@ async function spawnDynamicValidationInDev(
     implicitTags,
     nonce,
     renderOpts,
-    requestId,
     workStore,
   } = ctx
 
@@ -3317,7 +3343,6 @@ async function spawnDynamicValidationInDev(
               clientReferenceManifest={clientReferenceManifest}
               ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
               nonce={nonce}
-              requestId={requestId}
             />,
             {
               signal: finalClientReactController.signal,
@@ -3831,7 +3856,6 @@ async function prerenderToStream(
             clientReferenceManifest={clientReferenceManifest}
             ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
             nonce={nonce}
-            requestId={requestId}
           />,
           {
             signal: initialClientReactController.signal,
@@ -4065,7 +4089,6 @@ async function prerenderToStream(
                 clientReferenceManifest={clientReferenceManifest}
                 ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
                 nonce={nonce}
-                requestId={requestId}
               />,
               {
                 signal: finalClientReactController.signal,
@@ -4223,7 +4246,6 @@ async function prerenderToStream(
               clientReferenceManifest={clientReferenceManifest}
               ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
               nonce={nonce}
-              requestId={requestId}
             />,
             JSON.parse(JSON.stringify(postponed)),
             {
@@ -4330,7 +4352,6 @@ async function prerenderToStream(
           clientReferenceManifest={clientReferenceManifest}
           ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
           nonce={nonce}
-          requestId={requestId}
         />,
         {
           onError: htmlRendererErrorHandler,
@@ -4462,7 +4483,6 @@ async function prerenderToStream(
               clientReferenceManifest={clientReferenceManifest}
               ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
               nonce={nonce}
-              requestId={requestId}
             />,
             JSON.parse(JSON.stringify(postponed)),
             {
@@ -4547,7 +4567,6 @@ async function prerenderToStream(
           clientReferenceManifest={clientReferenceManifest}
           ServerInsertedHTMLProvider={ServerInsertedHTMLProvider}
           nonce={nonce}
-          requestId={requestId}
         />,
         {
           onError: htmlRendererErrorHandler,
@@ -4722,7 +4741,6 @@ async function prerenderToStream(
               preinitScripts={errorPreinitScripts}
               clientReferenceManifest={clientReferenceManifest}
               nonce={nonce}
-              requestId={requestId}
             />
           ),
           streamOptions: {
